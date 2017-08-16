7fbddf55036c5832de185cda57158384f08be48d
Bharath <bharath@llip.life>
2017-08-15 22:17:32 -0700

    Commiting again-Song reviewed

--- fileSys.js
+++ fileSys.js
@@ -1,11 +1,11 @@
 const fs= require('fs');
 
 const fileSys=(err,files)=>{
-  files.forEach(printOtherNames=(ele)=>{
-    if (ele != 'bharath'){
-      fs.writeFile('/home/bharath/gitcode/garagescript/files.txt',files+ "\n");
-    }
-  });
+  files.forEach(printOtherNames);
 }
-
+const printOtherNames=(eachName)=>{
+  if (eachName != 'bharath'){
+    fs.appendFile('/home/bharath/gitcode/garagescript/files.txt',eachName+ "\n");
+  }
+}
 fs.readdir('/home',fileSys);
